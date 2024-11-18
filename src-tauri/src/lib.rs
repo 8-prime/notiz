344f1d7a2e9074d92874168202338dd206cd3ecd
8-prime <hoffmeister.stefan99@gmail.com>
2024-11-18 22:35:22 +0100

    more basic setup stuff

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,36 @@
+use tauri::Window;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn minimize_window(window: Window) {
+    window.minimize().unwrap();
+}
+
+#[tauri::command]
+fn maximize_window(window: Window) {
+    window.maximize().unwrap();
+}
+
+#[tauri::command]
+fn close_window(window: Window) {
+    window.close().unwrap();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            minimize_window,
+            maximize_window,
+            close_window
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/components/titlebar.tsx
@@ -0,0 +1,41 @@
+import { invoke } from "@tauri-apps/api/core";
+import { Maximize, X } from "lucide-react";
+
+export default function Titlebar() {
+
+    const handleMinimize = async () => {
+        try {
+            await invoke('minimize_window');
+        } catch (error) {
+            console.error('Failed to minimize window:', error);
+        }
+    };
+    const handleMaximize = async () => {
+        try {
+            await invoke('maximize_window');
+        } catch (error) {
+            console.error('Failed to maximize window:', error);
+        }
+    };
+    const handleClose = async () => {
+        try {
+            await invoke('close_window');
+        } catch (error) {
+            console.error('Failed to close window:', error);
+        }
+    };
+
+    return (
+        <div data-tauri-drag-region className="w-screen h-10 flex justify-end items-center">
+            <button onClick={handleMinimize} >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="18" x2="19" y2="18"></line></svg>
+            </button>
+            <button onClick={handleMaximize} >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="7" x2="19" y2="7"></line></svg>
+            </button>
+            <button onClick={handleClose} >
+                <X />
+            </button>
+        </div>
+    );
+}
