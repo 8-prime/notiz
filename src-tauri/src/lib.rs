a74bc72d7267ce4c255c132d47c06e69aa452511
8-prime <hoffmeister.stefan99@gmail.com>
2024-11-19 21:24:00 +0100

    Beefing with tauri v2 to get window stuff working

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,4 @@
-use tauri::Window;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{tray::TrayIconBuilder, Manager, Window};
 
 #[tauri::command]
 fn minimize_window(window: Window) {
@@ -23,14 +17,53 @@ fn close_window(window: Window) {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // tauri::Builder::default().setup(|app| {
+    //     let tray = TrayIconBuilder::new().build(app)?;
+    //     Ok(())
+    // })
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            greet,
-            minimize_window,
-            maximize_window,
-            close_window
-        ])
+        .setup(|app| {
+            let tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .on_tray_icon_event(|tray, event| match event {
+                    tauri::tray::TrayIconEvent::Click {
+                        id,
+                        position,
+                        rect,
+                        button,
+                        button_state,
+                    } => {
+                        println!("left click pressed and released");
+                        // in this example, let's show and focus the main window when the tray is clicked
+                        let app = tray.app_handle();
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {}
+                })
+                .build(app)?;
+            let webview_window = tauri::WebviewWindowBuilder::new(
+                app,
+                "main",
+                tauri::WebviewUrl::App("index.html".into()),
+            )
+            .decorations(false)
+            .build()?;
+            webview_window.hide().unwrap();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    // tauri::Builder::default()
+    //     .plugin(tauri_plugin_shell::init())
+    //     .invoke_handler(tauri::generate_handler![
+    //         greet,
+    //         minimize_window,
+    //         maximize_window,
+    //         close_window
+    //     ])
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
 }
